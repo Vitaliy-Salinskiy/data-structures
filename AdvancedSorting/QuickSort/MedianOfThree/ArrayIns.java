c7481ef1d4393c4069e249026081ea46698164c4
Vitaliy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-08-21 17:03:48 +0200

    Add insertion sort to quick sort

--- AdvancedSorting/QuickSort/MedianOfThree/ArrayIns.java
+++ AdvancedSorting/QuickSort/MedianOfThree/ArrayIns.java
@@ -27,15 +27,14 @@ public class ArrayIns {
 
     public void recQuickSort(int left, int right) {
         int size = right - left + 1;
-        if (size <= 3) {
-            manualSort(left, right);
+        if (size < 10) {
+            insertionSort(left, right);
         } else {
             long median = medianOf3(left, right);
             int partition = partitionIt(left, right, median);
             recQuickSort(left, partition - 1);
             recQuickSort(partition + 1, right);
         }
-
     }
 
     public long medianOf3(int left, int right) {
@@ -103,6 +102,21 @@ public class ArrayIns {
             if (theArray[right - 1] > theArray[right]) {
                 swap(right - 1, right);
             }
+        }
+    }
+
+    public void insertionSort(int left, int right) {
+        int in, out;
+
+        for (out = left + 1; out <= right; out++) {
+            long temp = theArray[out];
+            in = out;
+
+            while (in > left && theArray[in - 1] >= temp) {
+                theArray[in] = theArray[in - 1];
+                --in;
+            }
+            theArray[in] = temp;
         }
     }
 
