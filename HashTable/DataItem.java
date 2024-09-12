14c3ca22848daf3eaa273e1d3a3235f9874a1923
Vitaliy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-09-12 06:41:56 +0200

    Implement Linear Probing Hash Table

---
+++ HashTable/DataItem.java
@@ -0,0 +1,13 @@
+package HashTable;
+
+public class DataItem {
+    private int iData;
+
+    public DataItem(int ii) {
+        iData = ii;
+    }
+
+    public int getKey() {
+        return iData;
+    }
+}
