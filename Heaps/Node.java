103aee30d50c070281008336a0bb8c2a54cd536a
Vitaliy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-09-14 05:02:39 +0200

    Implement Heap

---
+++ Heaps/Node.java
@@ -0,0 +1,17 @@
+package Heaps;
+
+public class Node {
+    private int iData;
+
+    public Node(int iData) {
+        this.iData = iData;
+    }
+
+    public int getKey() {
+        return iData;
+    }
+
+    public void setKey(int id) {
+        iData = id;
+    }
+}
