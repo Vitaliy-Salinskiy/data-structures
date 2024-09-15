8783559853b5027daacfea3d0f5d18b60c83c048
Vitaliy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-09-15 03:53:26 +0200

    Implement Graph with Depth First Search

---
+++ Graphs/Graph.java
@@ -0,0 +1,87 @@
+package Graphs;
+
+import java.util.Stack;
+
+public class Graph {
+    private final int MAX_VERTS = 20;
+    private Vertex[] vertexList;
+    private int[][] adjMat;
+    private int nVerts;
+    private Stack<Integer> stack;
+
+
+    public Graph() {
+        vertexList = new Vertex[MAX_VERTS];
+        adjMat = new int[MAX_VERTS][MAX_VERTS];
+        nVerts = 0;
+
+        for (int i = 0; i < MAX_VERTS; i++) {
+            for (int j = 0; j < MAX_VERTS; j++) {
+                adjMat[i][j] = 0;
+            }
+        }
+        stack = new Stack<>();
+    }
+
+    public void addVertex(char lab) {
+        vertexList[nVerts++] = new Vertex(lab);
+    }
+
+    public void addEdge(int start, int end) {
+        adjMat[start][end] = 1;
+        adjMat[end][start] = 1;
+    }
+
+    public int getAdjUnvisitedVertex(int v) {
+        for (int i = 0; i < nVerts; i++) {
+            if (adjMat[v][i] == 1 && !vertexList[i].wasVisited) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void dfs() {
+        vertexList[0].wasVisited = true;
+        displayVertex(0);
+        stack.push(0);
+
+        while (!stack.isEmpty()) {
+            int v = getAdjUnvisitedVertex(stack.peek());
+            if (v == -1) {
+                stack.pop();
+            } else {
+                vertexList[v].wasVisited = true;
+                displayVertex(v);
+                stack.push(v);
+            }
+        }
+
+        for (int i = 0; i < nVerts; i++) {
+            vertexList[i].wasVisited = false;
+        }
+    }
+
+    public void displayVertex(int v) {
+        System.out.print(vertexList[v].label);
+    }
+
+    public static void main(String[] args) {
+        Graph theGraph = new Graph();
+
+        theGraph.addVertex('A');
+        theGraph.addVertex('B');
+        theGraph.addVertex('C');
+        theGraph.addVertex('D');
+        theGraph.addVertex('E');
+
+        theGraph.addEdge(0, 1);
+        theGraph.addEdge(1, 2);
+        theGraph.addEdge(0, 3);
+        theGraph.addEdge(3, 4);
+
+        System.out.print("Visits: ");
+        theGraph.dfs();
+        System.out.println();
+    }
+}
---
+++ Graphs/Vertex.java
@@ -0,0 +1,11 @@
+package Graphs;
+
+public class Vertex {
+    public char label;
+    public boolean wasVisited;
+
+    public Vertex(char label) {
+        this.label = label;
+        wasVisited = false;
+    }
+}
