2e07b42ded32c55e562092d34a846e42a5bd19b5
Vitaliy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-09-18 18:40:28 +0200

    Implement Dijkstra’s algorithm to graph

---
+++ Graphs/WeightedGraphs/DistPar.java
@@ -0,0 +1,11 @@
+package Graphs.WeightedGraphs;
+
+public class DistPar {
+    public int distance;
+    public int parentVert;
+
+    public DistPar(int distance, int parentVert) {
+        this.distance = distance;
+        this.parentVert = parentVert;
+    }
+}
--- Graphs/WeightedGraphs/Graph.java
+++ Graphs/WeightedGraphs/Graph.java
@@ -8,6 +8,8 @@ public class Graph {
     private int nVerts;
     private int currentVert;
     private PriorityQ thePQ;
+    private DistPar[] sPath;
+    private int startToCurrent;
     private int nTree;
 
     public Graph() {
@@ -15,17 +17,23 @@ public class Graph {
 
         adjMat = new int[MAX_VERTS][MAX_VERTS];
         nVerts = 0;
+        nTree = 0;
 
         for (int i = 0; i < MAX_VERTS; i++) {
             for (int j = 0; j < MAX_VERTS; j++) {
                 adjMat[i][j] = INFINITY;
             }
         }
+        sPath = new DistPar[MAX_VERTS];
         thePQ = new PriorityQ();
     }
 
     public void addVertex(char label) {
         vertexList[nVerts++] = new Vertex(label);
+    }
+
+    public void addDirectedEdge(int start, int end, int weight) {
+        adjMat[start][end] = weight;
     }
 
     public void addEdge(int start, int end, int weight) {
@@ -37,7 +45,90 @@ public class Graph {
         System.out.print(vertexList[v].label);
     }
 
+    public void path() {
+        int startTree = 0;
+        vertexList[startTree].isInTree = true;
+        nTree = 1;
+
+        for (int i = 0; i < nVerts; i++) {
+            int tempDist = adjMat[startTree][i];
+            sPath[i] = new DistPar(tempDist, startTree);
+        }
+
+        while (nTree < nVerts) {
+            int indexMin = getMin();
+            int minDist = sPath[indexMin].distance;
+
+            if (minDist == INFINITY) {
+                System.out.println("There are unreachable vertices");
+                break;
+            } else {
+                currentVert = indexMin;
+                startToCurrent = sPath[indexMin].distance;
+            }
+
+            vertexList[currentVert].isInTree = true;
+            nTree++;
+            adjust_sPath();
+        }
+
+        displayPaths();
+
+        nTree = 0;
+        for (int i = 0; i < nVerts; i++) {
+            vertexList[i].isInTree = false;
+        }
+    }
+
+    public int getMin() {
+        int minDist = INFINITY;
+        int indexMin = 0;
+        for (int i = 1; i < nVerts; i++) {
+            if (!vertexList[i].isInTree && sPath[i].distance < minDist) {
+                minDist = sPath[i].distance;
+                indexMin = i;
+            }
+        }
+
+        return indexMin;
+    }
+
+    public void adjust_sPath() {
+        int column = 1;
+        while (column < nVerts) {
+            if (vertexList[column].isInTree) {
+                column++;
+                continue;
+            }
+
+            int currentToFringe = adjMat[currentVert][column];
+            int startToFringe = startToCurrent + currentToFringe;
+            int sPathDist = sPath[column].distance;
+
+            if (startToFringe < sPathDist) {
+                sPath[column].parentVert = currentVert;
+                sPath[column].distance = startToFringe;
+            }
+            column++;
+        }
+    }
+
+    public void displayPaths() {
+        for (int i = 0; i < nVerts; i++) {
+            System.out.print(vertexList[i].label + "=");
+            if (sPath[i].distance == INFINITY) {
+                System.out.print("inf");
+            } else {
+                System.out.print(sPath[i].distance);
+            }
+            char parent = vertexList[sPath[i].parentVert].label;
+            System.out.print("(" + parent + ") ");
+        }
+        System.out.println();
+    }
+
     public void mstw() {
+        nTree = 0;
         currentVert = 0;
 
         while (nTree < nVerts - 1) {
@@ -120,6 +211,27 @@ public class Graph {
 
         System.out.print("Minimum spanning tree: ");
         theGraph.mstw();
+        System.out.println();
+
+        Graph directedGraph = new Graph();
+
+        directedGraph.addVertex('A'); // 0
+        directedGraph.addVertex('B'); // 1
+        directedGraph.addVertex('C'); // 2
+        directedGraph.addVertex('D'); // 3
+        directedGraph.addVertex('E'); // 4
+
+        directedGraph.addDirectedEdge(0, 1, 50); // A -> B
+        directedGraph.addDirectedEdge(0, 3, 80); // A -> D
+        directedGraph.addDirectedEdge(1, 2, 60); // B -> C
+        directedGraph.addDirectedEdge(1, 3, 90); // B -> D
+        directedGraph.addDirectedEdge(2, 4, 40); // C -> E
+        directedGraph.addDirectedEdge(3, 2, 20); // D -> C
+        directedGraph.addDirectedEdge(3, 4, 70); // D -> E
+        directedGraph.addDirectedEdge(4, 1, 50); // E -> B
+
+        System.out.print("Shortest paths: ");
+        directedGraph.path();
         System.out.println();
     }
 }
